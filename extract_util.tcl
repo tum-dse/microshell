bd43b4fef9ff00ca86d044665d556a0849b2dd3f
halalboro <f20200947@pilani.bits-pilani.ac.in>
2026-05-11 12:43:55 +0000

    Add scripts + fallback bitstreams

---
+++ extract_util.tcl
@@ -0,0 +1,11 @@
+set proj_name [lindex $::argv 0]
+puts "$proj_name"
+
+open_checkpoint  build_${proj_name}_hw/checkpoints/shell_routed.dcp
+# update_compile_order -fileset sources_1
+
+# report_utilization -spreadsheet_file util_${proj_name}.xlsx
+# report_utilization -name util_1 -spreadsheet_file util_${proj_name}.xlsx
+report_utilization -hierarchical  -file util_${proj_name}.csv
+
+exit
--- program_fpga.sh
+++ program_fpga.sh
@@ -19,11 +19,14 @@ if [[ $1 == *"rdma"* ]]; then
   if [[ $host == *"clara"* ]]; then
     echo "Installing driver for clara."
     sudo insmod driver/coyote_drv.ko ip_addr_q0=0x0a000002 mac_addr_q0=000A350E24F2
+    sudo sysctl -w vm.nr_hugepages=1024
   elif [[ $host == *"amy"* ]]; then
     echo "Installing driver for amy."
     sudo insmod driver/coyote_drv.ko ip_addr_q0=0x0a000001 mac_addr_q0=000A350E24D6
+    sudo sysctl -w vm.nr_hugepages=1024
   fi
 else 
   echo "Host bitstream."
   sudo insmod driver/coyote_drv.ko
+  sudo sysctl -w vm.nr_hugepages=1024
 fi
